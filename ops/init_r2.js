cc5f28f36bb1137b68b6b326781a8ab959287e85
Igor <cyberskunk@gmail.com>
2024-07-10 14:48:15 +0300

    Try logging the error

--- ops/init_r2.js
+++ ops/init_r2.js
@@ -63,9 +63,13 @@ class SetupR2 {
 
   async setupPublicBucket() {
     const bucket = this.v.get('R2_PUBLIC_BUCKET');
-    console.log('R2_PUBLIC_BUCKET', bucket);
     !bucket && console.log('Could not read R2_PUBLIC_BUCKET');
+    try {
     await this._setupBucket(bucket);
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
     await this._setupCorsRules();
   }
 }
